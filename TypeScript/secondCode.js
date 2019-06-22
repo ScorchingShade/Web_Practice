ac533f91428aaa9cad2b381cec15f52f89328026
Ankush Sharma <ankushors789@gmail.com>
2019-06-21 23:54:37 -0700

    updated ts

--- TypeScript/secondCode.js
+++ TypeScript/secondCode.js
@@ -6,5 +6,18 @@ function exp1(a, b) {
     console.log(a);
     console.log(b);
 }
+function holaBhola() {
+    var num = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        num[_i] = arguments[_i];
+    }
+    var sum1 = 0;
+    var i;
+    for (i = 0; i < num.length; i++) {
+        sum1 = sum1 + num[i];
+    }
+    console.log(sum1);
+}
+holaBhola(32, 323, 232, 23, 23, 1);
 console.log(exp(1, 23));
 exp1(2);
--- TypeScript/secondCode.ts
+++ TypeScript/secondCode.ts
@@ -10,5 +10,17 @@ function exp1(a:number, b?:number){
     console.log(b);
 }
 
+function holaBhola(...num:number[]){
+    var sum1:number=0;
+    var i;
+
+    for(i=0;i<num.length;i++){
+        sum1=sum1+num[i];
+    }
+    console.log(sum1)
+}
+
+holaBhola(32,323,232,23,23,1);
+
 console.log(exp(1,23));
 exp1(2);
